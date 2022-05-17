c97681dafdf7315f8dd6e52fdc8d879295635897
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2022-05-17 11:06:11 -0700

    Python script executes through Java

--- src/main/java/com/drain/Controllers/min_and_max.py
+++ src/main/java/com/drain/Controllers/min_and_max.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys 
 
 # Find minumum and maximum of all test scores from "Scores" column
-df = pd.read_csv('example.csv') 
+df = pd.read_csv("C:\\Users\\akshay\\Desktop\\CSA\\CSA_Projects\\Drain-Gang\\src\\main\\java\\com\\drain\\Controllers\\example.csv") 
 
 print("The max of all scores are: " + str(df["Scores"].max()))
 print("The min of all scores are: " + str(df["Scores"].min()))
