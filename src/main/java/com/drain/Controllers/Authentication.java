63de77d444c3f226b1c6c97ccbde0b8362a17d1c
alvinzhengq <vortexdomainia@gmail.com>
2022-05-30 19:26:21 -0700

    working jwt login system

--- src/main/java/com/drain/Controllers/Authentication.java
+++ src/main/java/com/drain/Controllers/Authentication.java
@@ -1,7 +1,6 @@
 package com.drain.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +16,11 @@ public class Authentication {
     @PostMapping(value = "/login")
     @ResponseBody
     public String getJWT(@RequestParam String username, @RequestParam String password) throws SQLException {
-        
+        String token = JWT.create().withAudience(username).sign(Algorithm.HMAC256("development_secret"));
+
+        if (Database.checkCredentials(username, password)) {
+            return token;
+        }
 
         return "BAD CREDENTIALS";
     }
