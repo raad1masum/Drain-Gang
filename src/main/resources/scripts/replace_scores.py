e2704858fa9c603791d5e17536fb73ab3bde901f
Raadwan Masum <piraadwan@gmail.com>
2022-06-01 09:48:11 -0700

    fix save file

--- src/main/resources/scripts/replace_scores.py
+++ src/main/resources/scripts/replace_scores.py
@@ -3,6 +3,8 @@ import json
 import sys
 
 data = json.loads(sys.argv[1])
+data = json.loads(data)
+
 dataPath = sys.argv[2] + "/" + sys.argv[3]
 
 df = pd.DataFrame(columns=['Student ID', 'Scores'])
@@ -10,4 +12,4 @@ df = pd.DataFrame(columns=['Student ID', 'Scores'])
 for key, value in data.items():
     df = df.append({'Student ID': key, 'Scores': value}, ignore_index=True)
 
-df.to_csv(dataPath, index=False, sep=",")
+df.to_csv(dataPath, index=False, sep=",", mode="w+")
